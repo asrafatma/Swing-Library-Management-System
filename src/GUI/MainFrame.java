fc4df20a6f56cf1683b4d832c267c0560b9d1df5
albashzia <ahmedalbashzia@gmail.com>
2026-05-31 22:18:55 +0500

    Resolving the error in file path of background image of MainFrame

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -21,7 +21,7 @@ public class MainFrame implements ActionListener {
         mFrame = new CreateFrame();
         mSubmitButton = new CreateSubmitButton("LOGIN");
 
-        ImageIcon backgroundIcon = new ImageIcon(" Resources/image.jpeg");
+        ImageIcon backgroundIcon = new ImageIcon("Resources/image.jpeg");
         Image scaledImage = backgroundIcon.getImage().getScaledInstance(700, 500, Image.SCALE_SMOOTH);
         backgroundLabel = new JLabel(new ImageIcon(scaledImage));
         backgroundLabel.setBounds(0, 0, 700, 500);
